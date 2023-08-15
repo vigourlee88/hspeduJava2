fb78c2d1c6b90941a2a3f66384d935382e030b8a
vigourlee88 <108648123+vigourlee88@users.noreply.github.com>
2023-08-15 20:11:05 +0900

    springboot

---
+++ eclipse-workspace/springboot_05_maven_and_boot_profiles/src/main/java/com/itheima/Springboot05MavenAndBootProfilesApplication.java
@@ -0,0 +1,13 @@
+package com.itheima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot05MavenAndBootProfilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot05MavenAndBootProfilesApplication.class, args);
+	}
+
+}
