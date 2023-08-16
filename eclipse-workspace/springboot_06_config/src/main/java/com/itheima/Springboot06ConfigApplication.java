1bac5fcbb27eb7db2ab6a6104899303958b49231
vigourlee88 <108648123+vigourlee88@users.noreply.github.com>
2023-08-16 22:35:42 +0900

    springboot

---
+++ eclipse-workspace/springboot_06_config/src/main/java/com/itheima/Springboot06ConfigApplication.java
@@ -0,0 +1,13 @@
+package com.itheima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot06ConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot06ConfigApplication.class, args);
+	}
+
+}
