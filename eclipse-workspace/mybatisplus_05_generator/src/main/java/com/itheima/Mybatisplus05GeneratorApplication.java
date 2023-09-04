53491cb1069bab5e3845fdb413a996120b6027da
vigourlee88 <108648123+vigourlee88@users.noreply.github.com>
2023-09-05 01:56:40 +0900

    maven-mybatisplus-generator

---
+++ eclipse-workspace/mybatisplus_05_generator/src/main/java/com/itheima/Mybatisplus05GeneratorApplication.java
@@ -0,0 +1,16 @@
+package com.itheima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+
+@SpringBootApplication
+public class Mybatisplus05GeneratorApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Mybatisplus05GeneratorApplication.class, args);
+	}
+
+}
---
+++ eclipse-workspace/mybatisplus_05_generator/src/test/java/com/itheima/Mybatisplus05GeneratorApplicationTests.java
@@ -0,0 +1,17 @@
+package com.itheima;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class Mybatisplus05GeneratorApplicationTests {
+	
+     @Test
+     void contextLoads() {
+ 
+     }
+	
+}
