8347aeca4cb5415184bc9564d3f090e9ed6b3728
vigourlee88 <108648123+vigourlee88@users.noreply.github.com>
2023-09-05 01:49:09 +0900

    mybatisplus

--- eclipse-workspace/mybatisplus_04_dml/src/main/java/com/itheima/domain/User.java
+++ eclipse-workspace/mybatisplus_04_dml/src/main/java/com/itheima/domain/User.java
@@ -3,7 +3,9 @@ package com.itheima.domain;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableLogic;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.Version;
 
 import lombok.Data;
 
@@ -24,6 +26,13 @@ public class User {
 	
 	@TableField(exist = false)//字段不存在
 	private Integer online;
+	
+//	逻辑删除字段，标记当前记录是否被删除
+//	@TableLogic(value="0",delval="1")//配置文件中已配置
+	private Integer deleted;
 		
+	//实现乐观锁
+	@Version
+	private Integer version;
 
 }
--- eclipse-workspace/mybatisplus_04_dml/src/test/java/com/itheima/Mybatisplus04DmlApplicationTests.java
+++ eclipse-workspace/mybatisplus_04_dml/src/test/java/com/itheima/Mybatisplus04DmlApplicationTests.java
@@ -1,5 +1,8 @@
 package com.itheima;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -27,15 +30,46 @@ class Mybatisplus04DmlApplicationTests {
 	
 	@Test
 	void testDelete() {
-		userDao.deleteById(1698258223066251265L);
+//		List<Long> list=new ArrayList<>();
+//		list.add(1698253885501603842L);
+//		list.add(1698255605086552066L);
+//		list.add(1698256056964132866L);
+//		userDao.deleteBatchIds(list);
+		
+//		List<Long> list=new ArrayList<>();
+//		list.add(1L);
+//		list.add(3L);
+//		list.add(4L);
+//		userDao.selectBatchIds(list);
+		
+		
+		userDao.deleteById(2L);
+//		System.out.println(userDao.selectList(null));
 	}
 	
 	@Test
 	void testUpdate() {//提供哪些字段就修改哪些值，不提供就不修改
-		User user = new User();
-		user.setId(1L);
-		user.setName("Tom666");
-		user.setPassword("tom888");
+//		User user = new User();
+//		user.setId(3L);
+//		user.setName("Jock666");
+//		user.setVersion(1);//传入1传出2
+//		userDao.updateById(user);
+		
+//		//1.先通过要修改的数据id将当前数据查询出来
+//		User user = userDao.selectById(3L);
+//		//2.将要修改的属性逐一设置进去
+//		user.setName("Jock888");
+//		userDao.updateById(user);
+		
+		//1.先通过要修改的数据id将当前数据查询出来
+		User user = userDao.selectById(3L);//A用户version=3
+		
+		User user2 = userDao.selectById(3L);//B用户version=3
+		
+		user2.setName("Jock aaa");  //更新成功
+		userDao.updateById(user2);  //version=>4 ok
+		
+		user.setName("Jock bbb");   //version=3?条件还成立吗?不成立
 		userDao.updateById(user);
 		
 	}
