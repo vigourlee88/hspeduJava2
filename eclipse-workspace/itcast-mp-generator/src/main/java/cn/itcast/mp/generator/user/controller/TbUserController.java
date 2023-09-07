27eede2e415dab06e45d75a9cda6cc1e3840e7f8
vigourlee88 <108648123+vigourlee88@users.noreply.github.com>
2023-09-07 23:19:51 +0900

    maven-mp-generator

---
+++ eclipse-workspace/itcast-mp-generator/src/main/java/cn/itcast/mp/generator/user/controller/TbUserController.java
@@ -0,0 +1,18 @@
+package cn.itcast.mp.generator.user.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author itcast
+ * @since 2023-08-31
+ */
+@Controller
+@RequestMapping("/user/tb_user")
+public class TbUserController {
+
+}
