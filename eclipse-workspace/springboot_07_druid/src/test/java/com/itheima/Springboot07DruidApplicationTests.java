776f79aeb9622d3c010a334154151c1e146a6fc2
vigourlee88 <108648123+vigourlee88@users.noreply.github.com>
2023-09-17 16:17:19 +0900

    springboot_druid

---
+++ eclipse-workspace/springboot_07_druid/src/test/java/com/itheima/Springboot07DruidApplicationTests.java
@@ -0,0 +1,21 @@
+package com.itheima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.itheima.dao.BookDao;
+
+@SpringBootTest
+class Springboot07DruidApplicationTests {
+
+	@Autowired
+    private BookDao bookDao;
+   
+	@Test
+    void contextLoads() {
+        System.out.println(bookDao.getById(3));
+    }
+
+
+}
