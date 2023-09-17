e23b0c14e80688b6711676d0c4ddd0fb8cccb23d
vigourlee88 <108648123+vigourlee88@users.noreply.github.com>
2023-09-17 16:16:09 +0900

    springboot_mybatis_plus

---
+++ eclipse-workspace/springboot_06_mybatis_plus/src/test/java/com/itheima/Springboot06MybatisPlusApplicationTests.java
@@ -0,0 +1,26 @@
+package com.itheima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.itheima.dao.BookDao;
+
+@SpringBootTest
+class Springboot06MybatisPlusApplicationTests {
+
+	@Autowired
+    private BookDao bookDao;
+
+    @Test
+    void contextLoads() {
+        System.out.println(bookDao.selectById(2));
+    }
+
+    @Test
+    void testGetAll() {
+        System.out.println(bookDao.selectList(null));
+    }
+
+
+}
