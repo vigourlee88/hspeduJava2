936adcadd29fbc4e1d47a73fbf7c1693266fc4a2
vigourlee88 <108648123+vigourlee88@users.noreply.github.com>
2023-09-21 19:56:42 +0900

    springboot_profiles

---
+++ eclipse-workspace/springboot_10_profiles/src/main/java/com/itheima/Springboot10Config1Application.java
@@ -0,0 +1,13 @@
+package com.itheima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot10Config1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot10Config1Application.class, args);
+	}
+
+}
