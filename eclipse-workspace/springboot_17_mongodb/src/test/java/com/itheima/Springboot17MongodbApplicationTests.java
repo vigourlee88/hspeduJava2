2e19ff21fe3888abc48e88157cd94dda307a7247
vigourlee88 <108648123+vigourlee88@users.noreply.github.com>
2023-10-01 15:50:05 +0900

    springboot_mongodb

---
+++ eclipse-workspace/springboot_17_mongodb/src/test/java/com/itheima/Springboot17MongodbApplicationTests.java
@@ -0,0 +1,36 @@
+package com.itheima;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+import com.itheima.domain.Book;
+
+@SpringBootTest
+class Springboot17MongodbApplicationTests {
+
+	@Autowired
+    private MongoTemplate mongoTemplate;
+
+    @Test
+    void contextLoads() {
+        Book book = new Book();
+        book.setId(2);
+        book.setName("springboot2");
+        book.setType("springboot2");
+        book.setDescription("springboot2");
+
+        mongoTemplate.save(book);
+    }
+
+    @Test
+    void find(){
+        List<Book> all = mongoTemplate.findAll(Book.class);
+        System.out.println(all);
+    }
+
+
+}
