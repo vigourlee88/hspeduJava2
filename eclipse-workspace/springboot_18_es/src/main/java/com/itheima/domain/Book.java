0239e08c15854ea4d688f4f73b205df6b77228aa
DESKTOP-0GF91S7\jack <vigourlee137@gmail.com>
2023-10-19 23:47:20 +0900

    springboot_es

--- eclipse-workspace/springboot_18_es/src/main/java/com/itheima/domain/Book.java
+++ eclipse-workspace/springboot_18_es/src/main/java/com/itheima/domain/Book.java
@@ -5,8 +5,9 @@ import lombok.Data;
 
 @Data
 public class Book {
-    private Integer id;
-    private String type;
-    private String name;
-    private String description;
+	private Integer id;
+	private String type;
+	private String name;
+	private String description;
+
 }
